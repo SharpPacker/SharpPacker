6ac93348df57bc032841935443e7f081e4c9ec0b
krb3d <Rumpl.ZoL@Gmail.com>
2019-08-16 00:47:03 +0300

    EqualiseWeight() basic implementation

--- SharpPacker/Services/WeightRedistributor.cs
+++ SharpPacker/Services/WeightRedistributor.cs
@@ -3,17 +3,92 @@ using SharpPacker.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace SharpPacker.Services
 {
     class WeightRedistributor
     {
-        public List<Box4d> Boxes { get; set; }
+        private readonly List<Box4d> boxes;
 
         public WeightRedistributor(IEnumerable<Box4d> _boxes)
         {
-            Boxes = new List<Box4d>();
-            Boxes.AddRange(_boxes);
+            boxes = new List<Box4d>();
+            boxes.AddRange(_boxes);
+        }
+
+        /// <summary>
+        /// Attempt to equalise weight distribution between 2 boxes
+        /// </summary>
+        /// <param name="boxA"></param>
+        /// <param name="boxB"></param>
+        /// <param name="targetWeight"></param>
+        /// <returns>was the weight rebalanced?</returns>
+        private bool EqualiseWeight(ref PackedBox4d boxA, ref PackedBox4d boxB, float targetWeight)
+        {
+            var anyIterationSuccessful = false;
+
+            PackedBox4d overWeightBox;
+            PackedBox4d underWeightBox;
+
+            if(boxA.TotalWeight > boxB.TotalWeight)
+            {
+                overWeightBox = boxA;
+                underWeightBox = boxB;
+            } else
+            {
+                overWeightBox = boxB;
+                underWeightBox = boxA;
+            }
+
+            var overWeightBoxItems = overWeightBox.PackedItems.ToList();
+            var underWeightBoxItems = underWeightBox.PackedItems.ToList();
+
+            var i = overWeightBoxItems.Count - 1;
+            while(i >= 0)
+            {
+                var overWeightItem = overWeightBoxItems[i];
+                i--;
+
+                throw new Exception("check algorithm logic - why there is direct boxB using instead of over/underWeightBox?");
+                if(overWeightItem.Weight + boxB.TotalWeight > targetWeight)
+                {
+                    continue;
+                }
+
+                var newLighterBoxes = DoVolumeRepack(underWeightBoxItems, overWeightItem);
+                if(newLighterBoxes.Count != 1)
+                {
+                    continue; //only want to move this item if it still fits in a single box
+                }
+
+                underWeightBoxItems.Add(overWeightItem);
+
+                if(overWeightBoxItems.Count == 1)
+                { //sometimes a repack can be efficient enough to eliminate a box
+                    boxB = newLighterBoxes[0];
+                    boxA = null;
+
+                    return true;
+                } else
+                {
+                    overWeightBoxItems.RemoveAt(i+1);
+                    var newHeavierBoxes = DoVolumeRepack(overWeightBoxItems, null);
+                    if (newHeavierBoxes.Count != 1)
+                    {
+                        continue; //only want to move this item if it still fits in a single box
+                    }
+
+                    if (DidRepackActuallyHelp(boxA, boxB, newHeavierBoxes[0], newLighterBoxes[0]))
+                    {
+                        boxB = newLighterBoxes[0];
+                        boxA = newHeavierBoxes[0];
+                        anyIterationSuccessful = true;
+                    }
+                }
+
+            }
+            return anyIterationSuccessful;
         }
 
         /// <summary>
@@ -23,12 +98,39 @@ namespace SharpPacker.Services
         /// <returns></returns>
         private List<PackedBox4d> DoVolumeRepack(IEnumerable<Item4d> items)
         {
+            return DoVolumeRepack(items, null);
+        }
+
+        /// <summary>
+        /// Do a volume repack of a set of items.
+        /// </summary>
+        /// <param name="originalItems"></param>
+        /// <param name="plusOneItem"></param>
+        /// <returns></returns>
+        private List<PackedBox4d> DoVolumeRepack(IEnumerable<Item4d> originalItems, Item4d plusOneItem)
+        {
             var packer = new Packer();
-            packer.SetBoxes(this.Boxes);
-            packer.SetItems(items);
+            packer.SetBoxes(this.boxes);
+            packer.SetItems(originalItems);
+            if(plusOneItem != null)
+            {
+                packer.AddItem(plusOneItem);
+            }
 
             return packer.DoVolumePacking();
         }
+        
+        /// <summary>
+        /// Do a volume repack of a set of items.
+        /// </summary>
+        /// <param name="originalPackedItems"></param>
+        /// <param name="plusOnePackedItem"></param>
+        /// <returns></returns>
+        private List<PackedBox4d> DoVolumeRepack(IEnumerable<PackedItem4d> originalPackedItems, PackedItem4d plusOnePackedItem)
+        {
+            return DoVolumeRepack(originalPackedItems.Select(pi => pi.Item), plusOnePackedItem?.Item);
+        }
+
 
         /// <summary>
         /// Not every attempted repack is actually helpful - sometimes moving an item between two otherwise identical
