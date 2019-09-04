1f2c67515d80d654c6c1b19c4c82efad63d6dfc4
krb3d <Rumpl.ZoL@Gmail.com>
2019-09-05 00:38:54 +0300

    Additional tests implementation

---
+++ SharpPacker.Tests/BoxPackerTests/PackedBoxListTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SharpPacker.Tests.BoxPackerTests
+{
+    public class PackedBoxListTest
+    {
+        [Fact]
+        public void TestInsertAndCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void TestInsertFromArrayAndCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void TestTop()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void TestVolumeUtilisation()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void TestWeightVariance()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void TestMeanWeight()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
