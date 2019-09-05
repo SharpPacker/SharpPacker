9d3957c233b83f70aa9997905fff87a3579cc137
krb3d <Rumpl.ZoL@Gmail.com>
2019-09-05 22:12:27 +0300

    PackedBoxListTest implementation

--- SharpPacker.Tests/BoxPackerTests/PackedBoxListTest.cs
+++ SharpPacker.Tests/BoxPackerTests/PackedBoxListTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharpPacker.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -7,40 +8,163 @@ namespace SharpPacker.Tests.BoxPackerTests
 {
     public class PackedBoxListTest
     {
+        /// <summary>
+        /// Test that inserting individually correctly works.
+        /// </summary>
         [Fact]
         public void TestInsertAndCount()
         {
-            throw new NotImplementedException();
+            var box = Factory.CreateBox("Box", 10, 10, 10, 0, 10, 10, 10, 100);
+            var itemA = Factory.CreateItem("Item A", 5, 10, 10, 10, true);
+            var itemB = Factory.CreateItem("Item B", 5, 10, 10, 20, true);
+
+            var packedItemA = new PackedItem(itemA, 0, 0, 0, 5, 10, 10);
+            var packedItemB = new PackedItem(itemB, 0, 0, 0, 5, 10, 10);
+
+            var packedItemListA = new PackedItemList();
+            packedItemListA.Insert(packedItemA);
+            var packedBoxA = new PackedBox(box, packedItemListA);
+
+            var packedItemListB = new PackedItemList();
+            packedItemListB.Insert(packedItemB);
+            var packedBoxB = new PackedBox(box, packedItemListB);
+
+            var packedBoxList = new PackedBoxList();
+            packedBoxList.Insert(packedBoxA);
+            packedBoxList.Insert(packedBoxB);
+
+            Assert.Equal(2, packedBoxList.Count);
         }
 
+        /// <summary>
+        /// Test that inserting in bulk correctly works.
+        /// </summary>
         [Fact]
         public void TestInsertFromArrayAndCount()
         {
-            throw new NotImplementedException();
+            var box = Factory.CreateBox("Box", 10, 10, 10, 0, 10, 10, 10, 100);
+            var itemA = Factory.CreateItem("Item A", 5, 10, 10, 10, true);
+            var itemB = Factory.CreateItem("Item B", 5, 10, 10, 20, true);
+
+            var packedItemA = new PackedItem(itemA, 0, 0, 0, 5, 10, 10);
+            var packedItemB = new PackedItem(itemB, 0, 0, 0, 5, 10, 10);
+
+            var packedItemListA = new PackedItemList();
+            packedItemListA.Insert(packedItemA);
+            var packedBoxA = new PackedBox(box, packedItemListA);
+
+            var packedItemListB = new PackedItemList();
+            packedItemListB.Insert(packedItemB);
+            var packedBoxB = new PackedBox(box, packedItemListB);
+
+            var pBoxArray = new PackedBox[] { packedBoxA, packedBoxB };
+
+            var packedBoxList = new PackedBoxList();
+            packedBoxList.InsertFromArray(pBoxArray);
+
+            Assert.Equal(2, packedBoxList.Count);
         }
 
+        /// <summary>
+        /// Test we can peek at the "top" (next) item in the list.
+        /// </summary>
         [Fact]
         public void TestTop()
         {
-            throw new NotImplementedException();
+            var box = Factory.CreateBox("Box", 10, 10, 10, 0, 10, 10, 10, 100);
+            var itemA = Factory.CreateItem("Item A", 5, 10, 10, 10, true);
+            var itemB = Factory.CreateItem("Item B", 5, 10, 10, 20, true);
+
+            var packedItemA = new PackedItem(itemA, 0, 0, 0, 5, 10, 10);
+            var packedItemB = new PackedItem(itemB, 0, 0, 0, 5, 10, 10);
+
+            var packedItemListA = new PackedItemList();
+            packedItemListA.Insert(packedItemA);
+            var packedBoxA = new PackedBox(box, packedItemListA);
+
+            var packedItemListB = new PackedItemList();
+            packedItemListB.Insert(packedItemB);
+            var packedBoxB = new PackedBox(box, packedItemListB);
+
+            var pBoxArray = new PackedBox[] { packedBoxA, packedBoxB };
+
+            var packedBoxList = new PackedBoxList();
+            packedBoxList.Insert(packedBoxA);
+            packedBoxList.Insert(packedBoxB);
+
+            Assert.Equal(packedBoxA, packedBoxList.Top());
         }
 
         [Fact]
         public void TestVolumeUtilisation()
         {
-            throw new NotImplementedException();
+            var box = Factory.CreateBox("Box", 10, 10, 10, 0, 10, 10, 10, 10);
+            var item = Factory.CreateItem("Item", 5, 10, 10, 10, true);
+
+            var packedItem = new PackedItem(item, 0, 0, 0, 5, 10, 10);
+
+            var packedItemList = new PackedItemList();
+            packedItemList.Insert(packedItem);
+            var packedBox = new PackedBox(box, packedItemList);
+
+            var packedBoxList = new PackedBoxList();
+            packedBoxList.Insert(packedBox);
+
+            Assert.Equal(50f, packedBoxList.GetVolumeUtilisation());
         }
 
         [Fact]
         public void TestWeightVariance()
         {
-            throw new NotImplementedException();
+            var box = Factory.CreateBox("Box", 10, 10, 10, 0, 10, 10, 10, 100);
+            var itemA = Factory.CreateItem("Item A", 5, 10, 10, 10, true);
+            var itemB = Factory.CreateItem("Item B", 5, 10, 10, 20, true);
+
+            var packedItemA = new PackedItem(itemA, 0, 0, 0, 5, 10, 10);
+            var packedItemB = new PackedItem(itemB, 0, 0, 0, 5, 10, 10);
+
+            var packedItemListA = new PackedItemList();
+            packedItemListA.Insert(packedItemA);
+            var packedBoxA = new PackedBox(box, packedItemListA);
+
+            var packedItemListB = new PackedItemList();
+            packedItemListB.Insert(packedItemB);
+            var packedBoxB = new PackedBox(box, packedItemListB);
+
+            var pBoxArray = new PackedBox[] { packedBoxA, packedBoxB };
+
+            var packedBoxList = new PackedBoxList();
+            packedBoxList.Insert(packedBoxA);
+            packedBoxList.Insert(packedBoxB);
+
+            Assert.Equal(25, packedBoxList.GetWeightVariance());
         }
 
         [Fact]
         public void TestMeanWeight()
         {
-            throw new NotImplementedException();
+            var box = Factory.CreateBox("Box", 10, 10, 10, 0, 10, 10, 10, 100);
+            var itemA = Factory.CreateItem("Item A", 5, 10, 10, 10, true);
+            var itemB = Factory.CreateItem("Item B", 5, 10, 10, 20, true);
+
+            var packedItemA = new PackedItem(itemA, 0, 0, 0, 5, 10, 10);
+            var packedItemB = new PackedItem(itemB, 0, 0, 0, 5, 10, 10);
+
+            var packedItemListA = new PackedItemList();
+            packedItemListA.Insert(packedItemA);
+            var packedBoxA = new PackedBox(box, packedItemListA);
+
+            var packedItemListB = new PackedItemList();
+            packedItemListB.Insert(packedItemB);
+            var packedBoxB = new PackedBox(box, packedItemListB);
+
+            var pBoxArray = new PackedBox[] { packedBoxA, packedBoxB };
+
+            var packedBoxList = new PackedBoxList();
+            packedBoxList.Insert(packedBoxA);
+            packedBoxList.Insert(packedBoxB);
+
+            Assert.Equal(15, packedBoxList.GetMeanWeight());
         }
 
     }
