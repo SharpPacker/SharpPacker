ebc9148bb689158a0cdd5e23f47aa672d1033324
krb3d <Rumpl.ZoL@Gmail.com>
2019-09-13 23:22:56 +0300

    Adapter Convertors basic methods

---
+++ BoxPackerCloneAdapter/Convertors.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpPacker.Core.DataTypes;
+
+namespace BoxPackerCloneAdapter
+{
+    static class Convertors
+    {
+        public static BoxPackerClone.Models.Box BoxToBox(SharpPacker.Core.Models.BoxType bt)
+        {
+            return new BoxPackerClone.Models.Box
+                {
+                    Reference = bt.Name,
+
+                    EmptyWeight = bt.EmptyWeight,
+                    MaxWeight = bt.MaxWeight,
+
+                    InnerWidth = bt.InnerDimensions.sizeX,
+                    InnerLength = bt.InnerDimensions.sizeY,
+                    InnerDepth = bt.InnerDimensions.sizeZ,
+
+                    OuterWidth = bt.OuterDimensions.sizeX,
+                    OuterLength = bt.OuterDimensions.sizeY,
+                    OuterDepth = bt.OuterDimensions.sizeZ,
+                };
+        }
+
+        public static BoxPackerClone.Models.Item ItemToItem(SharpPacker.Core.Models.Item item)
+        {
+            return new BoxPackerClone.Models.Item
+            {
+                Description = item.Name,
+
+                Width = item.Dimensions.sizeX,
+                Length = item.Dimensions.sizeY,
+                Depth = item.Dimensions.sizeZ,
+
+                Weight = item.Weight,
+
+                KeepFlat = (item.AllowedRotations.Length == 1 && item.AllowedRotations[0] == Rotation.XYZ_to_XYZ)
+                            || (item.AllowedRotations.Length == 2
+                                && item.AllowedRotations.Contains(Rotation.XYZ_to_XYZ)
+                                && item.AllowedRotations.Contains(Rotation.XYZ_to_YXZ)),
+            };
+        }
+    }
+}
---
+++ BoxPackerCloneAdapter/Pack.cs
@@ -0,0 +1,52 @@
+﻿using BoxPackerClone;
+using SharpPacker.Core;
+using SharpPacker.Core.Models;
+using System;
+using System.Collections.Generic;
+
+namespace BoxPackerCloneAdapter
+{
+    public class Options
+    {
+        public int MaxBoxesToBalanceWeight = 12;
+    }
+
+    public class BoxPackerCloneAdapter : ABoxPacker<Options>
+    {
+        readonly Packer packer = new Packer();
+        Options options;
+
+        public BoxPackerCloneAdapter()
+        {
+        }
+
+        public override void Init(Options options)
+        {
+            this.options = options;
+            Console.WriteLine("BoxPackerCloneAdapter.Init()");
+        }
+
+        public override BoxPackerResult PackBoxes(IEnumerable<BoxType> boxes, IEnumerable<Item> items)
+        {
+            var packer = new Packer();
+            packer.MaxBoxesToBalanceWeight = options.MaxBoxesToBalanceWeight;
+
+            foreach(var box in boxes)
+            {
+                packer.AddBox(Convertors.BoxToBox(box));
+            }
+
+            foreach(var item in items)
+            {
+                packer.AddItem(Convertors.ItemToItem(item));
+            }
+
+            throw new NotImplementedException();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            Console.WriteLine($"BoxPackerCloneAdapter.Dispose({disposing})");
+        }
+    }
+}
